1f47fe61a7ebbdec0e8a5525c26e8f3dd8c4fadd
Md. Nafish Sadik <md.nafish.sadik@gmail.com>
2023-11-19 10:00:58 +0600

    Routes added and customized and Navber added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,19 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: "#8ecae6",
+          secondary: "#219ebc",
+          accent: "#023047",
+          neutral: "#ffb703",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 }
 
