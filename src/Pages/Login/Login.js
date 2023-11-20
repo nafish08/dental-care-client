ed027cf9468276152a82f12ad3c9d71c57ec8a12
Md. Nafish Sadik <md.nafish.sadik@gmail.com>
2023-11-20 20:05:05 +0600

    Firebase installed and customized

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-            <h1>This is Login</h1>
+        <div className='flex h-screen justify-center items-center'>
+            <div class="card w-96 bg-neutral-700 shadow-xl">
+                <div class="card-body">
+                    <h2 class="text-center text-2xl font-bold">Login</h2>
+                </div>
+            </div>
         </div>
     );
 };
