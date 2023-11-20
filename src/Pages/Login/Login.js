833ab98d10b3d33f4c188db9f5aaba486f9f53bf
Md. Nafish Sadik <md.nafish.sadik@gmail.com>
2023-11-20 20:18:29 +0600

    Google sign in added and geting user info

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Login = () => {
+
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+    if (user) {
+        console.log(user);
+    }
+
     return (
         <div className='flex h-screen justify-center items-center'>
-            <div class="card w-96 bg-neutral-700 shadow-xl">
-                <div class="card-body">
-                    <h2 class="text-center text-2xl font-bold">Login</h2>
+            <div className="card w-96 bg-neutral-700 shadow-xl">
+                <div className="card-body">
+                    <h2 className="text-center text-2xl font-bold">Login</h2>
+                    <div className="divider">OR</div>
+                    <button
+                        onClick={() => signInWithGoogle()}
+                        className="btn btn-outline btn-accent uppercase"
+                    >Continue with google</button>
                 </div>
             </div>
         </div>
