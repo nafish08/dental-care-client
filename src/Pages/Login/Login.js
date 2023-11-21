2e6e0734a419c5767a08a1b64ece1e110001e75a
Md. Nafish Sadik <md.nafish.sadik@gmail.com>
2023-11-21 09:45:17 +0600

    Praivate route set to appointment page to redirect user

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -4,7 +4,7 @@ import auth from '../../firebase.init';
 
 import { useForm } from "react-hook-form"
 import Loading from '../Shared/Loading';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 const Login = () => {
 
@@ -30,6 +30,9 @@ const Login = () => {
     }
 
     let signInError;
+    const navigate = useNavigate();
+    const location = useLocation();
+    let from = location.state?.from?.pathname || "/";
 
     if (loading || gLoading) {
         return <Loading></Loading>
@@ -40,7 +43,7 @@ const Login = () => {
     }
 
     if (user || gUser) {
-        console.log(user || gUser);
+        navigate(from, { replace: true });
     }
 
     return (
