c22a006644f3d034f697bff6fbefafd6afda4d35
Md. Nafish Sadik <md.nafish.sadik@gmail.com>
2023-11-21 10:06:24 +0600

    Login error fixed

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 
@@ -25,7 +25,6 @@ const Login = () => {
     } = useForm()
 
     const onSubmit = (data) => {
-        console.log(data)
         signInWithEmailAndPassword(data.email, data.password);
     }
 
@@ -34,16 +33,18 @@ const Login = () => {
     const location = useLocation();
     let from = location.state?.from?.pathname || "/";
 
+    useEffect(() => {
+        if (user || gUser) {
+            navigate(from, { replace: true });
+        }
+    }, [user, gUser, from, navigate])
+
     if (loading || gLoading) {
         return <Loading></Loading>
     }
 
     if (error || gError) {
         signInError = <p className='text-red-500'><small>{error?.message || gError?.message}</small></p>
-    }
-
-    if (user || gUser) {
-        navigate(from, { replace: true });
     }
 
     return (
