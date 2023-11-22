b0bac45df4377b41dfd72155f6248d5fc3d42eee
Md. Nafish Sadik <md.nafish.sadik@gmail.com>
2023-11-22 20:25:19 +0600

    Toast added to booking modal to show confirmation

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -39,6 +39,12 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
             .then(res => res.json())
             .then(data => {
                 console.log(data);
+                if (data.success) {
+                    toast(`Appointment is set, ${formattedDate} at ${slot}`);
+                }
+                else {
+                    toast.error(`You already have an appointment on ${data.booking?.date} at ${data.booking?.slot}`);
+                }
                 // To close modal
                 setTreatment(null);
             })
