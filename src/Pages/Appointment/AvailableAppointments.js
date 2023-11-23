7b32ad735450d7df590ddd289f6d76fba9f59604
Md. Nafish Sadik <md.nafish.sadik@gmail.com>
2023-11-23 22:56:59 +0600

    Auto load on date change

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -2,26 +2,28 @@ import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
 import Service from './Service';
 import BookingModal from './BookingModal';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading';
 
 const AvailableAppointments = ({ date, setDate }) => {
 
-    // Fetching data from http://localhost:5000/available file
-    const [services, setServices] = useState([]);
     const [treatment, setTreatment] = useState(null);
 
     const formattedDate = format(date, 'PP');
-    useEffect(() => {
-        fetch(`http://localhost:5000/available?date=${formattedDate}`)
-            .then(res => res.json())
-            .then(data => setServices(data));
-    }, [])
+    const { data: services, isLoading } = useQuery(['available', formattedDate], () => fetch(`http://localhost:5000/available?date=${formattedDate}`)
+        .then(res => res.json())
+    )
+
+    if (isLoading) {
+        return <Loading />
+    }
 
     return (
         <div className='py-10'>
             <h4 className='text-xl text-center text-secondary pb-10'>Available Appointments on {format(date, 'PP')}</h4>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
-                    services.map(service => <Service
+                    services?.map(service => <Service
                         key={service._id}
                         service={service}
                         setTreatment={setTreatment}
