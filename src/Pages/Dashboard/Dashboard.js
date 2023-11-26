d2d4e2da7d9b8e656d23bd819081a589e3181d62
Md. Nafish Sadik <md.nafish.sadik@gmail.com>
2023-11-26 09:13:44 +0600

    navbar item position fixed and dashbord updated

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,24 +1,38 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
+import auth from '../../firebase.init';
+// import useAdmin from '../../hooks/useAdmin';
 
 const Dashboard = () => {
+    const [user] = useAuthState(auth);
+    // const [admin] = useAdmin(user);
+
     return (
         <div class="drawer drawer-mobile">
-            <input id='my-drawer-2' type='checkbox' className='drawer-toggle' />
-            <div className='drawer-content flex flex-col items-center justify-center'>
-                <h2 className='text-5xl text-red-700'>Dashboard</h2>
+            <input id="dashboard-sidebar" type="checkbox" class="drawer-toggle" />
+            <div class="drawer-content">
+                <h2 className='text-2xl font-bold text-purple-500'>Welcome to your Dashboard</h2>
                 <Outlet></Outlet>
-                <label htmlFor="my-drawer-2" className='btn btn-neutral drawer-button lg:hidden'>Open Drawer</label>
             </div>
-            <div className='drawer-open'>
-                <label htmlFor='my-drawer-2' aria-label='close sidebar' className='drawer-overlay'></label>
-                <ul className='menu p-4 w-80 min-h-full bg-neutral-700 text-base-content'>
-                    <li><Link to="/dashboard">Service</Link></li>
-                    <li><Link to="/dashboard/review">Service</Link></li>
+            <div class="drawer-open">
+                <label for="dashboard-sidebar" class="drawer-overlay"></label>
+                <ul class="menu p-4 overflow-y-auto w-48 bg-base-100 text-base-content">
+                    {/* <!-- Sidebar content here --> */}
+                    <li><Link to="/dashboard">My Appointments</Link></li>
+                    <li><Link to="/dashboard/review">My Reviews</Link></li>
+                    <li><Link to="/dashboard/history">My History</Link></li>
+                    {/* {admin && <>
+                        <li><Link to="/dashboard/users">All Users</Link></li>
+                        <li><Link to="/dashboard/addDoctor">Add a Doctor</Link></li>
+                        <li><Link to="/dashboard/manageDoctor">Manage Doctors</Link></li>
+                    </>} */}
                 </ul>
+
             </div>
         </div>
     );
 };
+
 
 export default Dashboard;
