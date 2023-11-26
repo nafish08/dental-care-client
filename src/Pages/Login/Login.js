7fe82cd044ced85f4077b5736eab317afbde8b26
Md. Nafish Sadik <md.nafish.sadik@gmail.com>
2023-11-26 12:29:03 +0600

    Jwt token issued in login and signup page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -5,6 +5,7 @@ import auth from '../../firebase.init';
 import { useForm } from "react-hook-form"
 import Loading from '../Shared/Loading';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
+import useToken from '../../hooks/useToken';
 
 const Login = () => {
 
@@ -17,6 +18,8 @@ const Login = () => {
         loading,
         error,
     ] = useSignInWithEmailAndPassword(auth)
+
+    const [token] = useToken(user || gUser);
 
     const {
         register,
@@ -34,10 +37,10 @@ const Login = () => {
     let from = location.state?.from?.pathname || "/";
 
     useEffect(() => {
-        if (user || gUser) {
+        if (token) {
             navigate(from, { replace: true });
         }
-    }, [user, gUser, from, navigate])
+    }, [token, from, navigate])
 
     if (loading || gLoading) {
         return <Loading></Loading>
